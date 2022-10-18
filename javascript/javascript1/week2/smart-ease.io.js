7c5b46515caee0c16eedc1a1fd98440bffd1b433
cygap <113382267+Cygap@users.noreply.github.com>
2022-10-18 19:08:05 +0200

    first task

--- javascript/javascript1/week2/smart-ease.io.js
+++ javascript/javascript1/week2/smart-ease.io.js
@@ -1,1 +1,17 @@
-console.log("Hello!");
+const myFreeCodeCamp = "https://www.freecodecamp.org/sudar";
+
+//firt taks code
+
+function getFullname(firstName, lastName) {
+  return `${firstName} ${lastName}`;
+}
+const fullName1 = getFullname(
+  prompt("please enter the first name:"),
+  prompt("Please, enter the last name:")
+);
+alert(`The full name will be: ${fullName1}`);
+const fullName2 = getFullname(
+  prompt("please enter the first name:"),
+  prompt("Please, enter the last name:")
+);
+alert(`The full name will be: ${fullName2}`);
