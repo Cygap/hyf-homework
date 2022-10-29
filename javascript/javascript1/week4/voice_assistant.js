150c27a93c5ac123602ab7b16b79974dcae990f3
cygap <113382267+Cygap@users.noreply.github.com>
2022-10-29 21:33:40 +0200

    created a list of commands

--- javascript/javascript1/week4/voice_assistant.js
+++ javascript/javascript1/week4/voice_assistant.js
@@ -1,3 +1,30 @@
-const assistant = {};
+const assistant = {
+  "Hello my name is": "There is a command!",
+  "What is my name": "There is a command!",
+  "Add fishing to my todo": "There is a command!",
+  "Add singing in the shower to my todo": "There is a command!",
+  "Remove fishing from my todo": "There is a command!",
+  "What is on my todo?": "There is a command!",
+  "What day is it today?": "There is a command!",
+  "what is ": "There is a command!",
+  "Set a timer for ": "There is a command!",
+};
+const user = {
+  name: "",
+  toDo: {},
+};
+function getReply(command) {
+  let knownCommand = false;
+  console.log(Object.keys(assistant));
+  for (let key of Object.keys(assistant)) {
+    console.log(key);
+    if (command.includes(key)) {
+      console.log(assistant[key]);
+      knownCommand = true;
+      return assistant[key];
+    }
+  }
+  if (!knownCommand) console.log("Command not known :(");
+}
 
-function getReply(command) {}
+getReply("what is 2 * 2");
