b3e9a93cb40b86f3a24e80e48b04fd998a4c4579
cygap <113382267+Cygap@users.noreply.github.com>
2022-10-31 14:14:00 +0100

    added all the assigned functions but calc

--- javascript/javascript1/week4/voice_assistant.js
+++ javascript/javascript1/week4/voice_assistant.js
@@ -1,12 +1,9 @@
 const user = {
   name: "",
-  toDo: {},
+  toDo: [],
 };
 
 function newName(command, name) {
-  console.log(
-    `This is the newName function with matched pattern ${command} and a username = ${name}`
-  );
   if (user.name === "") {
     console.log(`Nice to meet you, ${name}`);
   } else {
@@ -25,16 +22,80 @@ function findName(command, name) {
     console.log(`Your name is ${user.name}`);
   }
 }
+
+function addToDo(command, activity) {
+  user.toDo.push(activity);
+  console.log(`Added task "${activity}" to your todo list`);
+}
+
+function removeToDo(command, activity) {
+  if (user.toDo.length) {
+    const item = user.toDo.indexOf(activity);
+    if (item === -1) {
+      console.log(
+        `Cannot delete anything, your todo doesn't have a task called: "${activity}"`
+      );
+    } else {
+      console.log(
+        `Removed "${user.toDo.splice(item, 1)}" task from your todo list`
+      );
+    }
+  } else
+    console.log("Cannot delete anything, your todo list is already empty!");
+}
+
+function displayToDo(command) {
+  if (user.toDo.length) {
+    console.log(`Your todo list:\n${user.toDo.join("\n")}`);
+  } else
+    console.log(
+      "Good job! Your todo list is already empty! Everything is done!"
+    );
+}
+
+function todayName() {
+  const today = new Date();
+  console.log(
+    `Today is ${today.toLocaleDateString("en-GB", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    })}`
+  );
+}
+
+function timer(command, amount, typeOfTime) {
+  let ms = 1000;
+  switch (typeOfTime) {
+    case "hour":
+    case "hours":
+      ms *= 60 * 60 * amount;
+      break;
+    case "minute":
+    case "minutes":
+      ms *= 60 * amount;
+      break;
+    case "second":
+    case "seconds":
+      ms *= amount;
+  }
+  alert(`Timer set for ${amount} ${typeOfTime}(s) milliseconds: ${ms}`);
+  setTimeout(() => {
+    alert("Time is up!");
+  }, ms);
+}
+
 const assistant = new Map([
   [/my\s+name\s+is\s+(\w+)/i, newName],
-  [/is\s+my\s+name\s*(\w+)?/i, "Find name"],
-  [/Add\s+((\w+\s+)+)to\s+my\s+todo/i, "todo add command!"],
-  [/Remove\s+((\w+\s+)+)from\s+my\s+todo/i, "todo remove command!"],
-  [/What\s+is\s+on\s+my\s+todo/i, "display todo!"],
-  ["What day is it today?", "There is a command!"],
+  [/is\s+my\s+name\s*(\w+)?/i, findName],
+  [/Add\s+((\w+\s+)*\w+)\s+to\s+my\s+todo/i, addToDo],
+  [/Remove\s+((\w+\s+)*\w+\b)\s+from\s+my\s+todo/i, removeToDo],
+  [/What\s+is\s+on\s+my\s+todo/i, displayToDo],
+  [/What\s+day\s+is\s+it\s+today/i, todayName],
   [
     /timer\s+for\s+(\d+(?:\.\d+)?)\s*(hour(?:s)?|minute(?:s)?|second(?:s)?)/i,
-    "timer command!",
+    timer,
   ],
   [/(-?\d+(?:\.\d+)?)\s*([-+*\/])\s*(-?\d+(?:\.\d+)?)/, "Calculator"],
 ]);
@@ -47,16 +108,19 @@ function getReply(command) {
       knownCommand = true;
       return reply(...command.match(key));
     }
-    //
-    // return assistant[key](command);
-    // }
   }
   if (!knownCommand) console.log("Command not known :(");
 }
 
 // getReply("what is 2.25    *    -1.2");
 getReply("Hi! my name is   Alex");
-// getReply("IS MY NAME alex?");
-// getReply("remove sfishing from    my todO!");
+getReply("IS MY NAME alex?");
+getReply("add pick kids up from school and kindergarden to my todo");
+getReply("remove fishing from    my todO!");
+getReply("What is on my todo?");
+getReply("remove pick kids up from school and kindergarden from my todo");
+getReply("remove fishing from    my todO!");
 // getReply("Set my timer for 5 hours!");
 getReply("Now my name is George");
+getReply("What day is it today?");
+getReply("Set a timer for 5 seconds");
