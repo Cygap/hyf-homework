36b11e3883e1f3ca7607b46d27514e6912635cf4
cygap <113382267+Cygap@users.noreply.github.com>
2022-10-31 12:11:30 +0100

    replaced an Object for commands with Map, plan to use RegEx as keys

--- javascript/javascript1/week4/voice_assistant.js
+++ javascript/javascript1/week4/voice_assistant.js
@@ -1,14 +1,14 @@
-const assistant = {
-  "Hello my name is": "There is a command!",
-  "What is my name": "There is a command!",
-  "Add fishing to my todo": "There is a command!",
-  "Add singing in the shower to my todo": "There is a command!",
-  "Remove fishing from my todo": "There is a command!",
-  "What is on my todo?": "There is a command!",
-  "What day is it today?": "There is a command!",
-  "what is ": "There is a command!",
-  "Set a timer for ": "There is a command!",
-};
+const assistant = new Map([
+  ["Hello my name is", "There is a command!"],
+  ["What is my name", "There is a command!"],
+  ["Add fishing to my todo", "There is a command!"],
+  ["Add singing in the shower to my todo", "There is a command!"],
+  ["Remove fishing from my todo", "There is a command!"],
+  ["What is on my todo?", "There is a command!"],
+  ["What day is it today?", "There is a command!"],
+  ["what is ", "There is a command!"],
+  ["Set a timer for ", "There is a command!"],
+]);
 const user = {
   name: "",
   toDo: {},
@@ -16,11 +16,12 @@ const user = {
 function getReply(command) {
   let knownCommand = false;
 
-  for (let key of Object.keys(assistant)) {
+  for (let [key, reply] of assistant) {
     if (command.includes(key)) {
-      console.log(assistant[key]);
+      console.log(`Key is "${key}"; value is "${reply}"`);
+
       knownCommand = true;
-      return assistant[key](command);
+      // return assistant[key](command);
     }
   }
   if (!knownCommand) console.log("Command not known :(");
