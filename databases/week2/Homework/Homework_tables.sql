8c217206a229d54862bbfcd5f09cb0bd75e8b164
cygap <113382267+Cygap@users.noreply.github.com>
2023-01-27 10:21:30 +0100

    implemented feedback

--- databases/week2/Homework/Homework_tables.sql
+++ databases/week2/Homework/Homework_tables.sql
@@ -2,103 +2,103 @@ USE Clinic_scheduling;
 
 CREATE TABLE
   Clinics (
-    clinic_id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     Clinic_name VARCHAR NOT NULL,
     Clinic_address VARCHAR NOT NULL,
-    PRIMARY KEY (clinic_id)
+    PRIMARY KEY (id)
   );
 
 -- a USER can either have work schedule for certain date or a regular working schedule for certain weekdays
 -- hence different kind of time-slots
 CREATE TABLE
   Working_hours (
+    id INT NOT NULL AUTO_INCREMENT,
     date DATE,
     weekday CHAR(3),
     begins DATETIME NOT NULL,
     ends DATETIME NOT NULL,
-    slot_id INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (slot_id)
+    PRIMARY KEY (id)
   );
 
 CREATE TABLE
   Users (
-    user_id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR NOT NULL,
     sex VARCHAR,
     address VARCHAR,
     phone INT NOT NULL,
     email VARCHAR NOT NULL,
     notes VARCHAR,
-    PRIMARY KEY (user_id)
+    PRIMARY KEY (id)
   );
 
 CREATE TABLE
   Medical_forms (
-    Form_id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     Form_name VARCHAR NOT NULL,
     Form_link VARCHAR NOT NULL,
     Form_description VARCHAR NOT NULL,
-    PRIMARY KEY (Form_id)
+    PRIMARY KEY (id)
   );
 
 -- each doctor and several other user roles have specific schedules
 CREATE TABLE
-  Doctors_have_working_hours (
-    schedule_id INT NOT NULL AUTO_INCREMENT,
+  Schedules (
+    id INT NOT NULL AUTO_INCREMENT,
     slot_id INT NOT NULL,
     user_id INT NOT NULL,
-    PRIMARY KEY (schedule_id),
-    FOREIGN KEY (slot_id) REFERENCES Working_hours (slot_id),
-    FOREIGN KEY (user_id) REFERENCES Users (user_id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (slot_id) REFERENCES Working_hours (id),
+    FOREIGN KEY (user_id) REFERENCES Users (id)
   );
 
 -- the 'main' entity for this database
 CREATE TABLE
   Visit (
+    id INT NOT NULL AUTO_INCREMENT,
     cancelled BOOLEAN,
     Shall_remind BOOLEAN,
     notes VARCHAR,
     Diagnosis VARCHAR,
-    visit_id INT NOT NULL AUTO_INCREMENT,
     schedule_id INT NOT NULL,
     doc_user_id INT NOT NULL,
     patient_user_id INT NOT NULL,
     room_id INT NOT NULL,
-    PRIMARY KEY (visit_id),
-    FOREIGN KEY (schedule_id) REFERENCES Doctors_have_working_hours (schedule_id),
-    FOREIGN KEY (doc_user_id) REFERENCES Users (user_id),
-    FOREIGN KEY (patient_user_id) REFERENCES Users (user_id),
-    FOREIGN KEY (room_id) REFERENCES Rooms (room_id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (schedule_id) REFERENCES Schedules (id),
+    FOREIGN KEY (doc_user_id) REFERENCES Users (id),
+    FOREIGN KEY (patient_user_id) REFERENCES Users (id),
+    FOREIGN KEY (room_id) REFERENCES Rooms (id)
   );
 
 CREATE TABLE
   Rooms (
-    room_id INT NOT NULL AUTO_INCREMENT,
-    room_name VARCHAR,
-    room_function VARCHAR,
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR,
+    function VARCHAR,
     clinic_id INT NOT NULL,
     spec_id INT NOT NULL,
-    PRIMARY KEY (room_id),
-    FOREIGN KEY (clinic_id) REFERENCES Clinics (clinic_id),
-    FOREIGN KEY (spec_id) REFERENCES Medical_specializations (spec_id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (clinic_id) REFERENCES Clinics (id),
+    FOREIGN KEY (spec_id) REFERENCES Medical_specializations (id)
   );
 
 CREATE TABLE
   Medical_specializations (
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR NOT NULL,
-    spec_id INT NOT NULL AUTO_INCREMENT,
     description VARCHAR(1024),
     required_role_id INT NOT NULL,
-    PRIMARY KEY (spec_id),
-    FOREIGN KEY (required_role_id) REFERENCES Roles (role_id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (required_role_id) REFERENCES Roles (id)
   );
 
 CREATE TABLE
   Roles (
+    id INT NOT NULL AUTO_INCREMENT,
     type VARCHAR NOT NULL,
-    role_id INT NOT NULL AUTO_INCREMENT,
     decription VARCHAR NOT NULL,
-    PRIMARY KEY (role_id)
+    PRIMARY KEY (id)
   );
 
 CREATE TABLE
@@ -106,8 +106,8 @@ CREATE TABLE
     user_id INT NOT NULL,
     role_id INT NOT NULL,
     PRIMARY KEY (user_id, role_id),
-    FOREIGN KEY (user_id) REFERENCES Users (user_id),
-    FOREIGN KEY (role_id) REFERENCES Roles (role_id)
+    FOREIGN KEY (user_id) REFERENCES Users (id),
+    FOREIGN KEY (role_id) REFERENCES Roles (id)
   );
 
 CREATE TABLE
@@ -128,8 +128,8 @@ CREATE TABLE
     viewer_role_id INT NOT NULL,
     viewes_roles_id INT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (viewer_role_id) REFERENCES Roles (role_id),
-    FOREIGN KEY (viewes_roles_id) REFERENCES Roles (role_id)
+    FOREIGN KEY (viewer_role_id) REFERENCES Roles (id),
+    FOREIGN KEY (viewes_roles_id) REFERENCES Roles (id)
   );
 
 CREATE TABLE
@@ -138,8 +138,8 @@ CREATE TABLE
     editor_role_id INT NOT NULL,
     edits_roles_id INT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (editor_role_id) REFERENCES Roles (role_id),
-    FOREIGN KEY (edits_roles_id) REFERENCES Roles (role_id)
+    FOREIGN KEY (editor_role_id) REFERENCES Roles (id),
+    FOREIGN KEY (edits_roles_id) REFERENCES Roles (id)
   );
 
 CREATE TABLE
@@ -147,6 +147,6 @@ CREATE TABLE
     user_id INT NOT NULL,
     spec_id INT NOT NULL,
     PRIMARY KEY (user_id, spec_id),
-    FOREIGN KEY (user_id) REFERENCES Users (user_id),
-    FOREIGN KEY (spec_id) REFERENCES Medical_specializations (spec_id)
+    FOREIGN KEY (user_id) REFERENCES Users (id),
+    FOREIGN KEY (spec_id) REFERENCES Medical_specializations (id)
   );
