9abd706a33e253b0925042d612a76e133fbcab6c
cygap <113382267+Cygap@users.noreply.github.com>
2023-02-03 10:05:24 +0100

    replaced arguments with userArguments to avoid namespace conflicts with reserved words

--- nodejs/week1/avg.js
+++ nodejs/week1/avg.js
@@ -2,13 +2,13 @@ console.log(getAverage(process.argv));
 
 /**
  *
- * @param {Array} arguments - list of arguments the user have entered. Presumes argument[0] and argument[1]
+ * @param {Array} userArguments - list of arguments the user have entered. Presumes argument[0] and argument[1]
  *  to always be system default peth-arguments, which are removed.
  * @returns {Number} - the average of provided arguments
  */
-function getAverage(arguments) {
-  arguments.splice(0, 2);
-  const args = arguments
+function getAverage(userArguments) {
+  userArguments.splice(0, 2);
+  const args = userArguments
     .map((parameter) => Number(parameter))
     .filter((parameter) => !isNaN(parameter));
   console.log(args);
