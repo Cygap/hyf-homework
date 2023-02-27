04b332308aba45712d1bafa1c70bc7b4862afa21
cygap <113382267+Cygap@users.noreply.github.com>
2023-02-27 09:49:16 +0100

    added no tasks notification

--- react/react1/week2/todo/src/components/TodoList.jsx
+++ react/react1/week2/todo/src/components/TodoList.jsx
@@ -5,11 +5,12 @@ import TodoContext from "./TodoContext";
 import AddTodo from "./AddTodo";
 
 export default function TodoList() {
-  const [tasks, tasksDispatch] = useContext(TodoContext);
+  const [tasks] = useContext(TodoContext);
 
   return (
     <div>
       <AddTodo />
+      {tasks.length ? undefined : "Congratulations! You've done everything!"}
       <ul>
         {tasks.map((task: object) => (
           <TodoItem task={task} key={task.id} id={`task-${task.id}`} />
